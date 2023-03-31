6d5a0c11d031910dcfbbe6bcb525a6b4d63527fa
Dmitry <grammattika@gmail.com>
2023-03-31 19:34:26 +0300

    Добавьте файлы проекта.

---
+++ Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableS
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const string MyName = "Dimaeeee";
+            Console.WriteLine(5);
+            Console.WriteLine('\x7F');
+            Console.WriteLine('\x7F');
+            Console.ReadKey();
+        }
+    }
+}
