1beb3d666ee50d5b5797bee842f13fc9315f121c
Dmitry <grammattika@gmail.com>
2023-04-01 10:18:52 +0300

    обновил

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 namespace VariableS{
     internal class Program{
         static void Main(){
+
             string _myName = "Dima";
-            int _age = 10;
+            int _age = 10; //лучше byte
             bool _pet = true;
             double _size = 39.5;
 
@@ -18,7 +19,11 @@ namespace VariableS{
             Console.WriteLine($"Do i have a Pet? {_pet}");
             Console.WriteLine($"My shoe size is {_size}");
 
+            Console.WriteLine("floatMin {0} ", float.MinValue);
+            Console.WriteLine("floatMax {0} ", float.MaxValue);
+
             Console.ReadKey();
         }
+
     }
 }
