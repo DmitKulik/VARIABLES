c05c7007ca1a89d335c3d4e2e00689d455f57363
Dmitry <grammattika@gmail.com>
2023-04-02 15:13:26 +0300

    Финальная практика модуля «Анкетируем пользователей»

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,36 +9,57 @@ using System.Threading.Tasks;
 namespace VariableS{
     internal class Program{
         static void Main(){
-          
 
-            string _myName = "Dima";
+
+            /*string _myName = "Dima";
             int _age = 10; //лучше byte
             bool _pet = true;
             double _size = 39.5;
+            string _favcolor = "Green";
+            */
+
+            // Console.WriteLine("Прочитать строку");
+            //string _str = Console.ReadLine();
+
+            //Финальная практика модуля «Анкетируем пользователей»
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.Write("You favorite day of week: ");
+            var day = (Week) int.Parse(Console.ReadLine());
+            Console.Write("Enter your birthdate: ");
+            var birthdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} Age is {1} Favorite day is {2} and birthdate {3}", name, age, day, birthdate);
+            Console.ReadKey();
 
 
+            // Console.WriteLine("Имя {0},\nВозраст {1}\nЛюбимый цвет {2}", _myName, _age, _favcolor);
 
-
-
-            Console.WriteLine($"My name is {_myName}");
-            Console.WriteLine($"My age is {_age}");
+            /*Console.WriteLine($"My name is {_myName}");
+            Console.WriteLine($"Мой возраст {_age}");
             Console.WriteLine($"Do i have a Pet? {_pet}");
             Console.WriteLine($"My shoe size is {_size}");
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.WriteLine("floatMin {0} ", float.MinValue);
             Console.WriteLine("floatMax {0} ", float.MaxValue);
+            */
 
-            Console.ReadKey();
+
+           // double result3 = 10 % 3;
+           //Console.WriteLine(result3);
+
+           // Console.ReadKey();
           
 
         }
 
 
-        enum Semaphore : int
+        enum Week : int
         {
-            Red = 100,
-            Yellow = 200,
-            Green = 300
+            Tuesday = 100,
+            Monday = 200,
+            Wednesday = 300
 
         }
 
